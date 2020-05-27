3e7ca12860476ce314a33ed854ecbb4bc55b78a4
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-27 16:56:13 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -4,7 +4,7 @@ def loop_message_five_times(message)
   count = 0
   while count < 5 do
     puts message
-    counter += 1
+    count += 1
   end
 end
 
@@ -12,7 +12,7 @@ def loop_message_n_times(message, number)
   count = 0
   while count < number do
     puts message
-    counter += 1
+    count += 1
   end
 end
 
